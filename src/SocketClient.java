7fc8b80c6f0e7e75d1b4457ffa3bc7629e6caa76
AdelaineDev <adelainesant18@gmail.com>
2023-09-10 17:39:21 -0300

    feat: implementa um socket client smtp

---
+++ src/SocketClient.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class SocketClient {
+    public static void main(String[] args) {
+        String serverOutlook = "SMTP.office365.com";
+        //Servidor SMTP do Microsoft
+        int portSMTP = 587 ;
+        //Porta de Saída
+
+        try (Socket socket = new Socket(serverOutlook, portSMTP)) {
+            System.out.println("------------- Criando conexão com servidor outlook -----------------");
+            System.out.println();
+
+            InputStream input = socket.getInputStream();
+            OutputStream output = socket.getOutputStream();
+
+            PrintWriter writer = new PrintWriter(output, true);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+
+            String line = reader.readLine();
+            System.out.println("------------- Servidor SMTP está pronto para continuar com sucesso -----------------");
+            System.out.println(line);
+            System.out.println();
+
+
+            writer.println(" ");
+            line = reader.readLine();
+       //     System.out.println("------------- Ação de e-mail solicitada correta, concluída -----------------");
+            System.out.println(line);
+            System.out.println();
+
+
+
+            writer.println("quit");
+            line = reader.readLine();
+            System.out.println("------------- Servidor SMTP fechando canal de transmissão-----------------");
+            System.out.println(line);
+            System.out.println();
+
+
+        } catch (UnknownHostException exp) {
+
+            System.out.println("O Servidor não foi encontrado: " + exp.getMessage());
+
+        } catch (IOException exp) {
+
+            System.out.println("Error: " + exp.getMessage());
+        }
+    }
+}
+
+
